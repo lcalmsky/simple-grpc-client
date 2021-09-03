a374a44e0f03d52d53be965577150d67673024de
Jaime <lcalmsky@gmail.com>
2021-09-04 03:26:07 +0900

    Initial commit

---
+++ src/test/java/io/lcalmsky/simplegrpcclient/SimpleGrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.simplegrpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGrpcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
